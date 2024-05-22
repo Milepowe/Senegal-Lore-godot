2cc3e773939acdb14553528e3edfd6ebcb9d2ce9
Bel <79281901+Bebebole@users.noreply.github.com>
2024-05-22 21:18:18 +0200

    j'ai ajouté la ndeye transition

--- scenes/menus/start_menu/intro_message.gd
+++ scenes/menus/start_menu/intro_message.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const main_menu = preload("res://scenes/menus/start_menu/main.tscn")
+
 signal pressedConfirm
 
 func _input(event):
@@ -16,10 +18,12 @@ func _ready():
 	await $Background/AnimationPlayer.animation_finished
 	$message.visible = true
 	
-	
 	await pressedConfirm
 	$message.visible = false
 	$Background/AnimationPlayer.play("out")
 	
 	await $Background/AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file("res://scenes/menus/start_menu/main.tscn")
+	
+	intro_msg_to_main_transition.transition()
+	await intro_msg_to_main_transition.transition_finished
+	get_tree().change_scene_to_packed(main_menu)
