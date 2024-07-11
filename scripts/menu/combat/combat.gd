7949eae7eae3f121fbd7a9fc4585238155d4e5ac
Bel <79281901+Bebebole@users.noreply.github.com>
2024-07-11 22:50:44 +0200

    added player infos + small text support

--- scripts/menu/combat/combat.gd
+++ scripts/menu/combat/combat.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#juste pour test la barrrrreeeee ouaaaaais
+	$PlayerInfos/HPbar.value = 50
+	$PlayerInfos/SPbar.value = 15
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
