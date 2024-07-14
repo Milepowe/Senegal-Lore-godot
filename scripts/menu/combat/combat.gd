3a80a144cb824b6d41a29d0c9cee84ac3aaf6bb2
Bel <79281901+Bebebole@users.noreply.github.com>
2024-07-14 20:52:03 +0200

    je me fait bully car askip j'ai mangé toute la viande mais the grind is real

--- scripts/menu/combat/combat.gd
+++ scripts/menu/combat/combat.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+var intro_speed = 300
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#juste pour test la barrrrreeeee ouaaaaais
-	$PlayerInfos/HPbar.value = 50
-	$PlayerInfos/SPbar.value = 15
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$Bilel.global_position = $Bilel.global_position.move_toward($PlayerPosition.global_position, delta*intro_speed)
+	$Missingno.global_position = $Missingno.global_position.move_toward($EnemyPosition.global_position, delta*intro_speed)
 	pass
