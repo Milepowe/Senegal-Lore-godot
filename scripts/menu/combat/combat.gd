f7046c767f4279037ae5eeb4ca553ee55f71f73c
Bel <79281901+Bebebole@users.noreply.github.com>
2024-07-17 17:35:05 +0200

    menu update, j'sais pas comment l'expliquer

--- scripts/menu/combat/combat.gd
+++ scripts/menu/combat/combat.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-var intro_speed = 300
+var intro_speed = 225
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Bilel.global_position = Vector2(-37, 153)
+	$Missingno.global_position = Vector2(480, 54)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
