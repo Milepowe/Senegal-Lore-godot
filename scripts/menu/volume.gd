cf0a512fce4c5efe2cdf0bbbeaf9a52bd8d67549
Milepowe <nilsbitho717@gmail.com>
2024-08-15 14:39:59 +0000

    volume slider finally works
    the script may be complicated but it's very simple

--- scripts/menu/volume.gd
+++ scripts/menu/volume.gd
@@ -1,6 +1,20 @@
 extends HSlider
 
 # ca marche pas donc allez fix jsp
-func music_slider(music_value):
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), music_value)
-	pass
+@export
+var bus_name: String
+
+var bus_index: int
+
+func _ready() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+	value_changed.connect(_on_value_changed)
+	value = db_to_linear(
+		AudioServer.get_bus_volume_db(bus_index)
+	)
+
+func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(
+		bus_index,
+		linear_to_db(value)
+)
